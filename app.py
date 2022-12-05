b2e9c7b286ecf0a92ee5733d201241448bd9533a
NovikSergey <serzh.novik.95@mail.ru>
2022-12-05 16:25:53 +0100

    add simple version of streamlit app

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+import pandas as pd 
+
+import plotly.graph_objects as go 
+import plotly.express as px 
+
+st.header("Hello world")
---
+++ streamlit_on_mac.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+if [ -d "./venv" ] 
+then
+    source './venv/bin/activate'
+    streamlit run app.py
+else
+    python3 -m virtualenv venv
+    source './venv/bin/activate'
+    pip install -r requirements.txt
+fi
+
