a372f7afa9f6c284812ad4fd633e7cb20af5f9c6
NovikSergey <serzh.novik.95@mail.ru>
2022-12-05 16:48:02 +0100

    add installation of virtualenv

--- streamlit_on_mac.sh
+++ streamlit_on_mac.sh
@@ -4,6 +4,7 @@ then
     source './venv/bin/activate'
     streamlit run app.py
 else
+    pip3 install virtualenv
     python3 -m virtualenv venv
     source './venv/bin/activate'
     pip install -r requirements.txt
